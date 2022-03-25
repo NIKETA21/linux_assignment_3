6a3bd55e0d8b3e74644558f49d3c0ea19123b6ba
NIKETA21 <84794703+NIKETA21@users.noreply.github.com>
2022-03-25 12:02:44 +0530

    Update question2_assignment3.c

--- question2_assignment3.c
+++ question2_assignment3.c
@@ -5,16 +5,31 @@
 #include<pthread.h>
 void* canc(void* ptr)
 {
+	int i, a=0;
+	pthread_t thread_id ;
+	thread_id =  pthread_self();
+	
+	for(i=0, i<6;i++)
+	{	 
 	printf("NIketadubey\n");
-	pthread_cancel (pthread_self());
+	a++;
+	}
+	if (a==4)
+	{	pritnf("pthread is cancel\n");
+		
+	 	pthread_cancel (pthread_self());
+	}
 	return 0;
 	
 }
 int main()
 {
 	pthread_t th;
+	printf("create the thread\n");
+	       
 	pthread_create(&th, NULL , canc, NULL);
-	//pthread_join(th , NULL);
+	pthread_join(th , NULL);
+	 pritnf("back to main\n");
 	return 0;
 	
 }
